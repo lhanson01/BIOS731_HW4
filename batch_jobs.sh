e42c3c139b373f6396f3dda4176327d4600788d3
Hanson <WHHANSO@emory.edu>
2026-04-12 12:19:02 -0400

    Added job sh script

---
+++ batch_jobs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --array=1-30
+#SBATCH --job-name=liam_731_sim
+#SBATCH --partition=wrobel
+#SBATCH --output=HW4_sim.out
+#SBATCH --error=HW4_sim.err
+
+module purge
+module load R
+
+JOBID=$SLURM_ARRAY_TASK_ID
+Rscript run_simulations.R $JOBID
