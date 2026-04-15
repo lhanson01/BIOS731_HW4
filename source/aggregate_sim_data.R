644770e1e74eb7363faf0838e2dfc49f57af8dd7
Hanson <WHHANSO@emory.edu>
2026-04-15 02:30:11 -0400

    Final commit for submission

--- source/aggregate_sim_data.R
+++ source/aggregate_sim_data.R
@@ -1,4 +1,4 @@
-load("./results/n_100_sim_1-50.RDA")
+library(purrr)
 
 n_chain <- 3
 results_paths <- here::here("results", list.files(here::here("results")))
@@ -9,10 +9,14 @@ for(path in results_paths){
   n <- length(results[[1]]$Gibbs$results_tib$labels[[1]])
   job_tib <- vector(mode="list", length = length(results))
   for(i in 1:length(results)){
-    sim_tib <- results[[i]]$Gibbs$results_tib
+    sim_tib_gibbs <- results[[i]]$Gibbs$results_tib %>%
+      rename_with(~ paste0(.x, "_gibbs"), starts_with("mu"))
+    vi_res <- as_tibble(t(c(results[[i]]$VI$means, results[[i]]$VI$comp_time) %>% 
+      `names<-`(c("mu_1_VI","mu_2_VI","mu_3_VI","mu_4_VI","comp_time_VI" ))))
     temp_list <- vector(mode = "list", length = n_chain)
     for(j in 1:length(temp_list)){
-      temp_list[[j]] <- sim_tib[1,] %>% mutate(chain = j)
+      print(j)
+      temp_list[[j]] <- bind_cols(sim_tib_gibbs[1,], vi_res) %>% mutate(chain = j)
     }
     job_tib[[i]] <- bind_rows(temp_list) %>% mutate(n = n)
   }
@@ -33,22 +37,96 @@ for(k in 1:4){
     )
 }
 
+# full_results_longer <- full_results_tib %>%
+#   pivot_longer(
+#     cols = matches("mu_(\\d+)_(.*)"),
+#     names_to = c("mu_k","Method"),
+#     names_pattern = "mu_(\\d+)_(.*)"
+#   )
+
 full_results_ag <- full_results_tib %>%
   select(-labels, -CIs) %>%
   group_by(n) %>%
-  summarise(mu_1_mean = mean(mu_1),
-            mu_2_mean = mean(mu_2),
-            mu_3_mean = mean(mu_3),
-            mu_4_mean = mean(mu_4),
-            mu_1_coverage = mean(`mu_1 Covers`),
-            mu_2_coverage = mean(`mu_2 Covers`),
-            mu_3_coverage = mean(`mu_3 Covers`),
-            mu_4_coverage = mean(`mu_4 Covers`),
-            mean_comp_time = mean(comp_time)) %>%
-  mutate(bias_mu_1 = mu_1_mean - mu_true[1],
-         bias_mu_2 = mu_2_mean - mu_true[2],
-         bias_mu_3 = mu_3_mean - mu_true[3],
-         bias_mu_4 = mu_4_mean - mu_true[4])
-  
+  summarise(mu_1_mean_gibbs = mean(mu_1_gibbs),
+            mu_2_mean_gibbs = mean(mu_2_gibbs),
+            mu_3_mean_gibbs = mean(mu_3_gibbs),
+            mu_4_mean_gibbs = mean(mu_4_gibbs),
+            mu_1_mean_VI = mean(mu_1_VI),
+            mu_2_mean_VI = mean(mu_2_VI),
+            mu_3_mean_VI = mean(mu_3_VI),
+            mu_4_mean_VI = mean(mu_4_VI),
+            mu_1_SE_gibbs = sd(mu_1_gibbs),
+            mu_2_SE_gibbs = sd(mu_2_gibbs),
+            mu_3_SE_gibbs = sd(mu_3_gibbs),
+            mu_4_SE_gibbs = sd(mu_4_gibbs),
+            mu_1_mean_VI = mean(mu_1_VI),
+            mu_2_mean_VI = mean(mu_2_VI),
+            mu_3_mean_VI = mean(mu_3_VI),
+            mu_4_mean_VI = mean(mu_4_VI),
+            mu_1_SE_VI = 3*sd(mu_1_VI),
+            mu_2_SE_VI = 3*sd(mu_2_VI),
+            mu_3_SE_VI = 3*sd(mu_3_VI),
+            mu_4_SE_VI = 3*sd(mu_4_VI),
+            mu_1_coverage = mean(`mu_1_gibbs Covers`),
+            mu_2_coverage = mean(`mu_2_gibbs Covers`),
+            mu_3_coverage = mean(`mu_3_gibbs Covers`),
+            mu_4_coverage = mean(`mu_4_gibbs Covers`),
+            mean_comp_time_gibbs = mean(comp_time),
+            mean_comp_time_VI = mean(comp_time_VI)) %>%
+  mutate(bias_mu_1_gibbs = mu_1_mean_gibbs - mu_true[1],
+         bias_mu_2_gibbs = mu_2_mean_gibbs - mu_true[2],
+         bias_mu_3_gibbs = mu_3_mean_gibbs - mu_true[3],
+         bias_mu_4_gibbs = mu_4_mean_gibbs - mu_true[4],
+         bias_mu_1_VI = mu_1_mean_VI - mu_true[1],
+         bias_mu_2_VI = mu_2_mean_VI - mu_true[2],
+         bias_mu_3_VI = mu_3_mean_VI - mu_true[3],
+         bias_mu_4_VI = mu_4_mean_VI - mu_true[4],
+         mu_1_coverage_SE = sqrt(mu_1_coverage*(1-mu_1_coverage)/n),
+         mu_2_coverage_SE = sqrt(mu_2_coverage*(1-mu_2_coverage)/n),
+         mu_3_coverage_SE = sqrt(mu_1_coverage*(1-mu_3_coverage)/n),
+         mu_4_coverage_SE = sqrt(mu_1_coverage*(1-mu_4_coverage)/n),
+         )
+
+
+bias_frame <- full_results_ag %>%
+  select(n, matches("bias"), matches("mu_\\d+_SE")) %>%
+  pivot_longer(cols = matches("bias_mu_\\d+"),
+               names_to = c("mu_k","Method"),
+               names_pattern = "bias_mu_(\\d+)_(.*)",
+               values_to = "Bias") %>%
+  mutate(n = factor(n))
+
+bias_plot <- ggplot(bias_frame, aes(fill = mu_k, y = Bias, x = n)) +
+         geom_col(position = "dodge", stat = "identity") + 
+  facet_wrap(vars(Method)) +
+  labs(title = "Bias by n and mu_k")
+
+ggsave("./output/bias_plot.png",
+       bias_plot)
+
+coverage_frame <- full_results_ag %>%
+  select(n, matches("coverage")) %>%
+  pivot_longer(cols = matches("coverage_SE$"), names_to = "k", 
+               names_pattern = "(.*)_coverage_SE", values_to = "coverage_SE") %>%
+  pivot_longer(cols = matches("coverage$"), names_to = "k2", 
+               names_pattern = "(.*)_coverage", values_to = "coverage") %>%
+  filter(k == k2) %>%
+  select(-k2) %>%
+  mutate(LL = coverage - 1.96*coverage_SE,
+         UL = coverage + 1.96*coverage_SE,
+         n = factor(n))
+
+cov_plot <- ggplot(coverage_frame, aes(fill = k, y = coverage, x = n)) +
+  geom_col(position = "dodge", stat = "identity") +
+  coord_cartesian(ylim=c(0.7,1)) +
+  geom_hline(yintercept = 0.95, linetype = "dashed") +
+  geom_errorbar(aes(ymin = LL, ymax = UL), 
+                  position = position_dodge(0.9), width = 0.5, color = "grey25") +
+  labs(title = "Coverage by mu and n")
+
+ggsave("./output/cov_plot.png",
+       cov_plot)
+
+
 
 
--- source/faithful_results.R
+++ source/faithful_results.R
@@ -1,9 +1,23 @@
+library(coda)
+library(dplyr)
+library(ggplot2)
+library(forecast)
+
+var_results <- readRDS(
+  file = "./output/faithful_var_results.rds")
+
+gibbs_results <- readRDS(
+  file = "./output/faithful_gibbs_results.rds")
+
+
 mu_chains_full <- data.frame(mu_1_chain_1 = gibbs_results$mu_hist[[1]][,1],
                              mu_1_chain_2 = gibbs_results$mu_hist[[2]][,1],
                              mu_1_chain_3 = gibbs_results$mu_hist[[3]][,1],
                              mu_2_chain_1 = gibbs_results$mu_hist[[1]][,2],
                              mu_2_chain_2 = gibbs_results$mu_hist[[2]][,2],
-                             mu_2_chain_3 = gibbs_results$mu_hist[[3]][,2]) %>%
+                             mu_2_chain_3 = gibbs_results$mu_hist[[3]][,2]) 
+
+mu_chains_full_long <- mu_chains_full %>%
   pivot_longer(
     cols = matches("mu_(\\d+)_chain_(\\d+)*"),
     names_to = c("mu","Chain"),
@@ -25,8 +39,8 @@ mu_chains_average <- mu_chains_full %>%
     names_to = c("mu","Chain"),
     names_pattern = "mu_(\\d+)_ch(\\d+)") 
 
-mu_1_chains <- mu_chains %>% filter(mu == 1)
-mu_2_chains <- mu_chains %>% filter(mu == 2)
+mu_1_chains <- mu_chains_average %>% filter(mu == 1)
+mu_2_chains <- mu_chains_average %>% filter(mu == 2)
 
 mu_1_trace_plot <- ggplot(data = mu_1_chains, aes(x = x, y = value, color = Chain) ) +
   geom_line(linewidth = 0.1) +
@@ -41,32 +55,63 @@ mu_2_trace_plot <- ggplot(data = mu_2_chains, aes(x = x, y = value, color = Chai
   xlab("Sample (averaged in groups of 10)")
 
 mu_1_lag_plot <- forecast::ggAcf(
-  mu_chains_full %>% filter(mu == 1, Chain == 1) %>% select(value),
+  mu_chains_full_long %>% filter(mu == 1, Chain == 1) %>% select(value),
   type = "correlation") + 
   labs(title = "Mu 1 Chain 1 lag plot") + ylab("Correlation")
 
-mu_2_lag_plot <- forecast::ggAcf(
-  mu_chains_full %>% filter(mu == 2, Chain == 1) %>% select(value),
+
+mu_2_chain_2_lag_plot <- forecast::ggAcf(
+  mu_chains_full_long %>% filter(mu == 2, Chain == 2) %>% select(value),
   type = "correlation") + 
-  labs(title = "Mu 2 Chain 1 lag plot") + ylab("Correlation")
+  labs(title = "Mu 2 Chain 2 lag plot") + ylab("Correlation")
 
 
 chain1_ess <- effectiveSize(gibbs_results$mu_hist[[1]])
 chain2_ess <- effectiveSize(gibbs_results$mu_hist[[2]])
 chain3_ess <- effectiveSize(gibbs_results$mu_hist[[1]])
 
+# Variational 
+
+var_est <- var_results$means
+var_labels <- var_results$labels
+var_ct <- var_results$comp_time
+
+# Gibbs
+
+gibbs_est <- colMeans(cbind(gibbs_results$results_tib$mu_1,
+                            gibbs_results$results_tib$mu_2))
+gibbs_ct <- mean(gibbs_results$results_tib$comp_time)
+
+gibbs_labels <- gibbs_results$results_tib$labels
+names(gibbs_labels) <- c("chain 1", "chain 2", "chain 3")
+gibbs_label_means <- rowMeans(bind_rows(gibbs_labels))
+
+
+result_sum_frame <- data.frame("Computation Time" = c(var_ct, gibbs_ct),
+                               "Mu 1" = c(var_est[1], gibbs_est[1]),
+                               "Mu 2" = c(var_est[2], gibbs_est[2]))
+
+rownames(result_sum_frame) <- c("CAVI", "Gibbs Sampler")
+
+
+
+# Agreement table for labels
+var_gibbs_conf_mat <- table(var_labels, gibbs_label_means)
+
+
+
 ggsave(
   mu_1_trace_plot,
   file = "./output/mu_1_trace_plot.png",
-  width = 12,
-  height = 5
+  width = 9,
+  height = 4
 )
 
 ggsave(
   mu_2_trace_plot,
   file = "./output/mu_2_trace_plot.png",
-  width = 12,
-  height = 5
+  width = 9,
+  height = 4
 )
 
 ggsave(
@@ -75,6 +120,6 @@ ggsave(
 )
 
 ggsave(
-  mu_2_lag_plot,
+  mu_2_chain_2_lag_plot,
   file = "./output/mu_2_lag_plot.png"
 )
